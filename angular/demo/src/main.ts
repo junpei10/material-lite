2f06e388b487b8441e66e72c3d7b31d9ff33b461
Junpei <52122683+junpei10@users.noreply.github.com>
2020-12-06 08:42:59 +0900

    update

--- angular/demo/src/main.ts
+++ angular/demo/src/main.ts
@@ -1,13 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { MlTheme, MlTheming, ML_INDIGO_PINK_PALETTE, ML_LIGHT_THEME } from '@material-lite/angular/core';
-import firebase from 'firebase/app';
 
 import { AppModule } from './app/app.module';
-import { firebaseConfig } from './db/firebase-config';
 import { environment } from './environments/environment';
 
-// firebase.initializeApp(firebaseConfig);
 const myTheme: MlTheme = {
   ...ML_LIGHT_THEME,
   primaryContainer: 'white',
